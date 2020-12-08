2e6469aaeb70243af93581e0972db0a84b644846
Matthew Castleberry <mtc0030@auburn.edu>
2020-12-08 10:43:09 -0600

    Rewrote package for icm

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # ros_icm20948
+
+A ROS node for communicating with the [ICM20948](https://www.sparkfun.com/products/15335) 9DOF IMU.
+
+## Description
+
+The node communicates with the ICM20948 via i2c on the Raspberry Pi using Sparkfun's Python library: https://github.com/sparkfun/Qwiic_9DoF_IMU_ICM20948_Py.  The i2c address is preset to 0x69 which is the default.  The data is stored in the following:
+
+* Accelerometer and Gyroscope (sensor_msgs/Imu): `icm20948/raw`
+* Magnometer (sensor_msgs/MagneticField): `icm20948/mag`
+* Temperature (sensor_msgs/Temperature): `icm20948/temp`
+* Diagnostics (diagnostic_msgs/DiagnosticStatus): `icm20948/status`
+
+## Installation Instructions
+
+* Enable i2c
+  ```
+  sudo apt-get install i2c-tools
+  i2cdetect -l
+  ```
+* Add `i2c-devl` to boot with `sudo nano /etc/modules-load.d/modules.conf`
+* Install wiringpi `sudo apt install wiringpi`
+* Connect i2c devices to Sparkfun Qwiic hat and run `i2cdetect -y 1` to identify channels
+* Install driver for ICM20948 IMU: `sudo pip3 install sparkfun-qwiic-icm20948`.
+
+## Running the Node
+
+* Option 1: `rosrun ros_icm20948 talker.py` 
+* Option 2: `roslaunch launches/icm20948.launch`
+  
+## Tested Setup
+
+It should work on other versions but Python 3 is a requirement.
+
+* Platform: Raspberry Pi 4
+* OS: Ubuntu MATE 20.04
+* ROS: Noetic
+* Python: 3.8.5
+
--- src/talker.py
+++ src/talker.py
@@ -1,59 +1,86 @@
 #!/usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import Temperature,FluidPressure
+from sensor_msgs.msg import Temperature,MagneticField,Imu
 from std_msgs.msg import Float64
+from diagnostic_msgs.msg import DiagnosticStatus
 import time
-import board
-import busio
-import adafruit_mpl3115a2
+import qwiic_icm20948
+import sys
 
+def icm20948_node():
 
-def mpl3115a2_node():
-     # Initialize the I2C bus.
-    i2c = busio.I2C(board.SCL, board.SDA)
-
-    # Initialize the MPL3115A2.
-    sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
-    # Alternatively you can specify a different I2C address for the device:
-    #sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x10)
-
-    # You can configure the pressure at sealevel to get better altitude estimates.
-    # This value has to be looked up from your local weather forecast or meteorlogical
-    # reports.  It will change day by day and even hour by hour with weather
-    # changes.  Remember altitude estimation from barometric pressure is not exact!
-    # Set this to a value in pascals:
-    sensor.sealevel_pressure = 102250
-
-    temp_pub = rospy.Publisher('mpl3115a2/temperature', Temperature, queue_size=10)
-    press_pub = rospy.Publisher('mpl3115a2/pressure', FluidPressure, queue_size=10)
-    alt_pub = rospy.Publisher('mpl3115a2/altitude', Float64, queue_size=10)
-    rospy.init_node('mpl3115a2')
+    # Initialize ROS node
+    raw_pub = rospy.Publisher('icm20948/raw', Imu, queue_size=10)
+    mag_pub = rospy.Publisher('icm20948/mag', MagneticField, queue_size=10)
+    temp_pub = rospy.Publisher('icm20948/temp', Temperature, queue_size=10)
+    status_pub = rospy.Publisher('icm20948/status', DiagnosticStatus, queue_size=10)
+    rospy.init_node('icm20948')
     rate = rospy.Rate(1)
-    rospy.loginfo(rospy.get_caller_id() + "  mpl3115a2 node launched.")
+    rospy.loginfo(rospy.get_caller_id() + "  icm20948 node launched.")
+
+    IMU = qwiic_icm20948.QwiicIcm20948()
+
+    while IMU.connected == False:
+        message = "The Qwiic ICM20948 device isn't connected to the system. Please check your connection"
+        rospy.loginfo(message)
+        status_msg = DiagnosticStatus()
+        status_msg.level = 2
+        status_msg.name = "icm20948 IMU"
+        status_msg.message = message
+        status_pub.publish(status_msg)        
+
+    IMU.begin()    
 
     while not rospy.is_shutdown():
+        raw_msg = Imu()
+        raw_msg.header.stamp = rospy.Time.now()
+
+        raw_msg.orientation.w = 0
+        raw_msg.orientation.x = 0
+        raw_msg.orientation.y = 0
+        raw_msg.orientation.z = 0
+
+        raw_msg.linear_acceleration.x = IMU.axRaw
+        raw_msg.linear_acceleration.y = IMU.ayRaw
+        raw_msg.linear_acceleration.z = IMU.azRaw
+
+        raw_msg.angular_velocity.x = IMU.gxRaw
+        raw_msg.angular_velocity.y = IMU.gyRaw
+        raw_msg.angular_velocity.z = IMU.gzRaw
+
+        raw_msg.orientation_covariance[0] = -1
+        raw_msg.linear_acceleration_covariance[0] = -1
+        raw_msg.angular_velocity_covariance[0] = -1
+
+        raw_pub.publish(raw_msg)
+
+        mag_msg = MagneticField()
+        mag_msg.header.stamp = rospy.Time.now()
+        mag_msg.magnetic_field.x = IMU.mxRaw
+        mag_msg.magnetic_field.y = IMU.myRaw
+        mag_msg.magnetic_field.z = IMU.mzRaw
+        mag_msg.magnetic_field_covariance[0] = -1
+        mag_pub.publish(mag_msg)
+
         temp_msg = Temperature()
         temp_msg.header.stamp = rospy.Time.now()
-        temp_msg.temperature = 0.0
+        temp_msg.temperature = IMU.tmpRaw
         temp_msg.variance = 0.0
         temp_pub.publish(temp_msg)
         
-        press_msg = FluidPressure()
-        press_msg.header.stamp = rospy.Time.now()
-        press_msg.fluid_pressure = sensor.pressure
-        press_msg.variance = 0.0
-        press_pub.publish(press_msg)
+        status_msg = DiagnosticStatus()
+        status_msg.level = 0
+        status_msg.name = "icm20948 IMU"
+        status_msg.message = ""
+        status_pub.publish(status_msg)
 
-        alt_msg = sensor.altitude
-        alt_pub.publish(alt_msg)
-
-        rate.sleep()
-
-    rospy.loginfo(rospy.get_caller_id() + "  mpl3115a2 node finished")
+        rate.sleep()   
+    
+    rospy.loginfo(rospy.get_caller_id() + "  icm20948 node finished")
 
 if __name__ == '__main__':
     try:
-        mpl3115a2_node()
+        icm20948_node()
     except rospy.ROSInterruptException:
         rospy.loginfo(rospy.get_caller_id() + "  mpl3115a2 node exited with exception.")
